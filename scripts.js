1b68931929c1457b27e8d7ef1672970155affc28
Keith Moore <keith.m4@gmail.com>
2016-03-08 12:07:46 -0500

    Added message for taken square
    Message is displayed using innerHTML if taken squares are clicked.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,45 @@
-var player = "X"; // starts with Player 1 (Xs) 
+// make an array of arrays specifying winning board combos
+var winners = [
+	['a1','a2','a3'],
+	['b1','b2','b3'],
+	['c1','c2','c3'],
+	['a1','b1','c1'],
+	['a2','b2','c2'],
+	['a3','b3','c3'],
+	['a1','b2','c3'],
+	['a3','b2','c1']
+];
 
-function changePlayers(element) {
-	if (player === "X" || player === "") {
-		player = "O";
-		element.value = "Player 2";
-
-	} else {
-		player = "X";
-		element.value = "Player 1";
-	}
-
-	console.log(player);
-}
+var player = "X"; // starts with Player 1 (Xs)
+var playerLabel = document.getElementById("game-header");
+playerLabel.innerHTML = "Player 1";
 
 function makeMove(element) {
+	var playerLabel = document.getElementById("game-header");
+	
 	if (element.value === "") {
+		playerLabel.style.backgroundColor = "#009999";
 		element.value = player;
+		
+		if (player === "X") {
+		player = "O";
+		playerLabel.innerHTML = "Player 2";
+		} else {
+			player = "X";
+			playerLabel.innerHTML = "Player 1";
+		}
 	} else {
-		console.log("Don't do that.");
+		playerLabel.style.backgroundColor = "red";
+		playerLabel.innerHTML = "That square is taken.";
 	}
-
-	console.log(element.value);
 }
 
 function startOver() {
 	// returns to Player 1
 	player = "X";
-	var playerButton = document.getElementById('player-button');
-	playerButton.value = "Player 1";
+	var gameHeader = document.getElementById("game-header");
+	gameHeader.innerHTML = "Player 1";
+	gameHeader.style.backgroundColor = "#009999";
 	
 	// clears all squares
 	var squares = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
