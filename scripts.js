02b2edc99ac6f8b06a0dc99e23da3253c17f7fc4
Keith Moore <keith.m4@gmail.com>
2016-03-21 13:36:25 -0400

    Fixed square height issue

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 var playerOneMarkings = [];
 var playerTwoMarkings = [];
 var computer;
-var playerMode;
 var whosTurn = 1;
 var winners = [
 	['a1','a2','a3'],
@@ -14,18 +13,25 @@ var winners = [
 	['c1','b2','a3']
 	];
 
+/* var winners = [];
+var gridSize = 5;
+for (i=0; i<gridSize; i++) {
+	for (j=0; j<gridSize; i++) {
+		winners[i].push('a' + j);
+	}
+}
+console.log(winners); */
+
 $(document).ready(function(){
-	$('button').click(function() {
+	$('.buttony').click(function() {
 		var clickedButton = $(this).attr('id');
 		var squares = $('.square');
 		if (clickedButton == 'one-player') {
 			computer = true;
-			playerMode = 1;
 			squares.html('');
 			enableBoard();
 		} else if (clickedButton == 'two-players') {
 			computer = false;
-			playerMode = 2;
 			squares.html('');
 			enableBoard();
 		} else if (clickedButton == 'start-over') {
@@ -50,19 +56,22 @@ $(document).ready(function(){
 
 	$('.square').click(function(){
 		var element = $(this);
+		var elementID = element.attr('id');
 		var gameHeader = $('#game-header');
 		if (element.html() == '') {
 			//It's X's turn. So, we have an empty square, and it's X's turn. Put an X in.
 			if (whosTurn === 1) {
 				element.html('X');
+				element.addClass('blink');
 				element.removeClass('empty');
 				element.addClass('playerOneHasThisSpace');
-				playerOneMarkings.push(element.id);
-				whosTurn = 2;
 				gameHeader.html("player two's turn");
 				gameHeader.removeClass('player-one');
 				gameHeader.addClass('player-two');
+				playerOneMarkings.push(elementID);
+				console.log(playerOneMarkings);
 				checkWin();
+				whosTurn = 2;
 				if (checkWin() === true) {
 					return; }
 				if (computer) {
@@ -72,11 +81,12 @@ $(document).ready(function(){
 			gameHeader.removeClass('player-two');
 			gameHeader.addClass('player-one');
 			element.html('O');
+			element.addClass('blink');
 			element.removeClass('empty');
 			element.addClass('playerTwoHasThisSpace');
-			playerTwoMarkings.push(element.id);
-			whosTurn = 1;
+			playerTwoMarkings.push(elementID);
 			checkWin();
+			whosTurn = 1;
 		} else {
 			gameHeader.html("This box is taken.");
 			gameHeader.addClass('red'); }
@@ -88,7 +98,7 @@ function enableBoard() {
 	var gameHeader = $('#game-header');
 	gameHeader.html("player one's turn");
 	gameHeader.addClass('player-one');
-	gameHeader.addClass('player-two');
+	gameHeader.removeClass('player-two');
 	$('button').prop('disabled', false);
 }
 
@@ -97,16 +107,18 @@ function computersTurn() {
 	var emptySquares = $('.empty');
 	var randomEmptySquare = Math.floor(Math.random() * emptySquares.length);
 	var element = emptySquares[randomEmptySquare];
+	var elementID = element.getAttribute('id');
 	element.innerHTML = 'O';
+	element.classList.add('blink');
 	gameHeader.html('');
 	gameHeader.html("player one's turn");
 	gameHeader.addClass('player-one');
 	gameHeader.removeClass('player-two');
 	element.classList.remove('empty');
 	element.classList.add('playerTwoHasThisSpace');
-	playerTwoMarkings.push(element.id);
-	whosTurn = 1;
+	playerTwoMarkings.push(elementID);
 	checkWin();
+	whosTurn = 1;
 }
 
 function checkWin() {
@@ -123,14 +135,15 @@ function checkWin() {
 		for (j=0; j<thisWinCombination.length; j++) {
 			if(playerOneMarkings.indexOf(thisWinCombination[j]) > -1) {
 				playerOneRowCount++; // if the the index is not -1, then it's in there
+				console.log(playerOneRowCount);
 			} else if (playerTwoMarkings.indexOf(thisWinCombination[j]) > -1) {
 				playerTwoRowCount++;
 			}
+		if (playerOneRowCount === 3 || playerTwoRowCount === 3) {
+			gameOver(thisWinCombination);
+			return true; // we need to break out of the outer for-loop when one of the row count variables gets to 3, otherwise the counter will reset
+			} 
 		}
-	if (playerOneRowCount === 3 || playerTwoRowCount === 3) {
-		gameOver(thisWinCombination);
-		return true; // we need to break out of the outer for-loop when one of the row count variables gets to 3, otherwise the counter will reset
-		} 
 	}
 }
 
@@ -140,6 +153,7 @@ function gameOver(combo) {
 	for (i=0;i<combo.length;i++) {
 		document.getElementById(combo[i]).classList.add('winner');
 	}
+
 	if (theWinner === "X") {
 			gameHeader.removeClass('player-two');
 			gameHeader.addClass('player-one');
@@ -161,8 +175,7 @@ function gameOver(combo) {
 	$('#start-over').prop('disabled', false);
 }
 
-var squareWidth = document.getElementById('a1').clientWidth;
-var squares = document.getElementsByClassName('square');
-for(i=0; i<squares.length; i++){
-	squares[i].style.height = squareWidth + 'px';
-}
+var squareWidth = $('.left').width();
+var squares = $('.square');
+squares.height((squareWidth*0.5) + 'px');
+console.log(squares.height(squareWidth + 'px'));
